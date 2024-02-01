8428e890f18a6ef8d6e6c39ade07e5be48cdba91
Vladimir nett00n Budylnikov <git@nett00n.org>
2024-02-02 00:50:59 +0400

    add docstrings

--- bot/main.py
+++ bot/main.py
@@ -10,10 +10,9 @@ from telegram.ext import (
 from transliterate import translit
 from urllib.parse import quote
 
-
 TOKEN = os.environ.get("BOT_TOKEN")
 
-# Mapping of language codes to flags
+
 LANGUAGE_FLAGS = {
     "en": "🇬🇧",
     "ru": "🇷🇺",
@@ -22,24 +21,24 @@ LANGUAGE_FLAGS = {
 
 
 def start(update: Update, context: CallbackContext) -> None:
+    """Handler for the /start command."""
     update.message.reply_text(
         "Welcome to the Kartuli Reverse Transliterate Bot! Send me Latin text, and I will convert it to Georgian."
     )
 
 
 def generate_translate_link(language_code: str, georgian_url: str) -> str:
+    """Generate translation link for a specific language."""
     return f"https://translate.yandex.com/?lang=ka-{language_code}&text={georgian_url}"
 
 
 def latin_to_georgian(update: Update, context: CallbackContext) -> None:
+    """Convert Latin text to Georgian and provide translation links."""
     try:
         latin_text = update.message.text
         georgian_text = translit(latin_text, "ka")
         georgian_url = quote(georgian_text, safe="")
 
-        translate_languages = ["en", "ru", "uk"]
-
-        # Send the plain Georgian text in the first message
         response_message = (
             f"You can copy text from here for paste it in your favorite translate app:\n"
             f"```\n{georgian_text}\n```\n"
@@ -48,14 +47,14 @@ def latin_to_georgian(update: Update, context: CallbackContext) -> None:
             response_message, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True
         )
 
-        for language_code in translate_languages:
+        for language_code, flag in LANGUAGE_FLAGS.items():
             translate_link = generate_translate_link(
                 language_code, georgian_url)
             response_message = (
-                f"\n- {LANGUAGE_FLAGS[language_code]} {language_code.upper()} "
+                f"\n- {flag} {language_code.upper()} "
                 f"[Google.Translate]({translate_link})"
             )
-            # Send each link as a separate message
+
             update.message.reply_text(
                 response_message, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True
             )
@@ -66,6 +65,7 @@ def latin_to_georgian(update: Update, context: CallbackContext) -> None:
 
 
 def main() -> None:
+    """Main function to initialize and run the bot."""
     try:
         if TOKEN is None:
             raise ValueError("BOT_TOKEN environment variable is not set.")
